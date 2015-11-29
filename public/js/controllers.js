1d4429fb60b75c01a9daff0db95d8eae53a26501
WhiteDeath820 <allensam0@gmail.com>
2015-11-28 23:36:20 -0700

    sidebarnav

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,5 +5,15 @@ webctrl.controller('homeCtrl', function() {
 });
 
 webctrl.controller('leftTabCtrl', function() {
+    //this is the content located in the sidebar
+
+    var leftabcontent = [{
+        class: 'test',
+        content: 'test'
+    }, {
+        class: 'test2',
+        content: 'testw'
+    }];
+    $scope.products = leftabcontent;
 
 });
